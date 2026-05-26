3d8913574a20a1f683c70d5e1f75b2a006f0efde
Vsevolod <vstimf@gmail.com>
2026-05-26 09:50:07 +0300

    Stimer comment update

--- stimer/stimer.c
+++ stimer/stimer.c
@@ -6,7 +6,6 @@
 
 static volatile uint32_t TimeMs = 0;
 
-// Обработчик прерывания (вызывается каждые 10 мс)
 STIMER_ISR
 {
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
@@ -61,33 +60,24 @@ static void stimer_hw_init(void)
 {
     ATOMIC_BLOCK(ATOMIC_RESTORESTATE)
     {
-
-        // 1. Сбрасываем все настройки Timer1
         TCCR1A = 0;
         TCCR1B = 0;
-        TCNT1  = 0;           // счётчик в 0
+        TCNT1  = 0;          
 
-        // 2. Устанавливаем режим CTC (Clear Timer on Compare Match)
-        // WGM13:10 = 0100 → CTC с OCR1A
+        // CTC (Clear Timer on Compare Match)
         TCCR1B |= (1 << WGM12);
 
-        // 3. Предделитель
-        TCCR1B |= (1 << CS11);                            //8       Предделитель = 8 → частота таймера = 3686000 / 8 = 460750 Гц
-        //TCCR1B |= (1 << CS11) | (1 << CS10);            //64      Предделитель = 64 → частота таймера = 3686000 / 64 = 57600 Гц
-        //TCCR1B |= (1 << CS12);                          //256     Предделитель = 256 → частота таймера = 3686000 / 256 = 14398 Гц
+        // Divider (TimerFS = CoreFs / Divider)
+        TCCR1B |= (1 << CS11);                            // 8
+        //TCCR1B |= (1 << CS11) | (1 << CS10);            // 64
+        //TCCR1B |= (1 << CS12);                          // 256
 
-        // 5. Вычисляем значение для OCR1A
-        // 8 Период одного тика = 1 / 460750 ≈ 2.170 мкс
-        // 64 Период одного тика = 1 / 57600 ≈ 17.361 мкс
-        // 256 Период одного тика = 1 / 14398 ≈ 69.454 мкс
-
-        // Нужно 1 мс = 1000 мкс
-        // Количество тиков = 1000 / 2.170 ≈ 461
-        // → OCR1A = 460 (так как счёт начинается с 0 → 0..460 = 461 тик)
-
+        // Calculate OCR1A
+        // TickTime = 1/TimerFS
+        // OCR1A = (TimerPeriodMS*1000 / TickTime) - 1
         OCR1A = 460;
 
-        // 4. Разрешаем прерывание по совпадению с OCR1A
+        // 4. Enable OCR1A compare interrupt
         TIMSK |= (1 << OCIE1A);
     }
 }
@@ -95,6 +85,6 @@ static void stimer_hw_init(void)
 
 void stimer_init(void)
 {
-    TimeMs = 0;	// Обнуляем переменную времени. 
-    stimer_hw_init();  // Запускаем аппаратный таймер
+    TimeMs = 0;	
+    stimer_hw_init();
 }
